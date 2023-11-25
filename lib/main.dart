acfd0d9bbeaabeefa2fd8bbd38a4a7b7813c59d2
Gereon Elvers <gereonelvers99@gmail.com>
2023-11-25 01:49:56 +0100

    Trigger pipeline

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,6 @@ class MyCustomScrollBehavior extends MaterialScrollBehavior {
 class ValueVoyage extends StatelessWidget {
   const ValueVoyage({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
